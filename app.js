fd015fe2ccdb5a95de800751aaa82d9d74045f1a
Good Vibes <sethhixon1992@gmail.com>
2021-10-04 23:43:51 -0500

    added function for Inquire

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const pageHTML = generatePage(name, github);
+inquirer
+.prompt([
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  }
+])
+.then(answers => console.log(answers));
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+//const [name, github] = profileDataArgs;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//fs.writeFile('index.html', generatePage(name, github), err => {
+  //if (err) throw err;
+
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
